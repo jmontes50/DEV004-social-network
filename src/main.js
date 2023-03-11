15d2ef5dd7e293e243ba326fc79e70957f60f5ce
Jorge Montesinos <osmarmontes50@gmail.com>
2023-03-11 17:02:41 -0500

    prtobanding

--- src/main.js
+++ src/main.js
@@ -5,26 +5,35 @@
 // myFunction();
 
 const ROUTES = {
-    home: 'Home',
-    feed: 'Feed',
-    register: 'Register',
-    login: 'Login'
+    '/': '<h1>Home</h1><a href="login" data-link>Link</a>',
+    'feed': 'Feed',
+    'register': 'Register',
+    'login': 'Login'
 }
 
-function changePage(route){
+function changePage(route = '/'){
     const rootDiv = document.getElementById('root');
     rootDiv.innerHTML = ROUTES[route]
 }
 
+changePage()
+
 window.addEventListener("click", event => {
     const link = event.target.closest("a[data-link]");
+    console.log({link})
     if (link) {
       event.preventDefault();
       const href = link.getAttribute("href");
+      console.log({href})
       history.pushState(null, null, href);
     }
   });
 
-window.addEventListener('popstate', () => {
-  changePage(window.location.pathname)
-})
+  window.onpopstate = function(){
+    console.log('works')
+  }
+// window.addEventListener('popstate', () => {
+//   console.log('works')
+//   console.log(window.location.pathname)
+//   changePage(window.location.pathname)
+// })
