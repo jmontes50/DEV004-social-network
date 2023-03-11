9cf4bf49df810b58251497f24c1eb899700b2724
Jorge Montesinos <osmarmontes50@gmail.com>
2023-03-10 21:06:35 -0500

    probanding

--- src/main.js
+++ src/main.js
@@ -1,5 +1,30 @@
 // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
+// import { myFunction } from './lib/index.js';
 
-myFunction();
+// myFunction();
+
+const ROUTES = {
+    home: 'Home',
+    feed: 'Feed',
+    register: 'Register',
+    login: 'Login'
+}
+
+function changePage(route){
+    const rootDiv = document.getElementById('root');
+    rootDiv.innerHTML = ROUTES[route]
+}
+
+window.addEventListener("click", event => {
+    const link = event.target.closest("a[data-link]");
+    if (link) {
+      event.preventDefault();
+      const href = link.getAttribute("href");
+      history.pushState(null, null, href);
+    }
+  });
+
+window.addEventListener('popstate', () => {
+  changePage(window.location.pathname)
+})
